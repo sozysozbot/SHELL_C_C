fe77c9bc9343cce08d2db8b30da9d985990dd97d
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2023-07-16 15:39:53 +0900

    トークナイザとコード出力を分離

--- compiler.sh
+++ compiler.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 echoerr() { echo "$@" 1>&2; }
 
+input=$1
+num_buffer=''
+for (( i=0; i<${#input}; i++ )); do
+  char="${input:$i:1}"
+  if [[ "$char" = '0' || "$char" = '1' || "$char" = '2' || "$char" = '3' || "$char" = '4' || "$char" = '5' || "$char" = '6' || "$char" = '7' || "$char" = '8' || "$char" = '9' ]]
+  then
+    num_buffer+=$char 
+  elif [[ "$char" = '+' ]]
+  then
+    tokens+=($num_buffer +)
+    num_buffer=''
+  elif [[ "$char" = '-' ]]
+  then
+    tokens+=($num_buffer -)
+    num_buffer=''
+  else
+    echoerr "detected an unknown character '$char'"
+  fi
+done
+
+tokens+=($num_buffer)
+echoerr ${tokens[@]}
+
 echo '.intel_syntax noprefix'
 echo '.globl main'
 echo ''
 echo 'main:'
 echo '        mov rax, 0'
 
-foo=$1
+input=$1
 num_buffer=''
 previous_mnemonics='add'
-for (( i=0; i<${#foo}; i++ )); do
-  char="${foo:$i:1}"
+for (( i=0; i<${#input}; i++ )); do
+  char="${input:$i:1}"
   if [[ "$char" = '0' || "$char" = '1' || "$char" = '2' || "$char" = '3' || "$char" = '4' || "$char" = '5' || "$char" = '6' || "$char" = '7' || "$char" = '8' || "$char" = '9' ]]
   then
     echoerr "detected a number '$char'"
@@ -19,14 +42,12 @@ for (( i=0; i<${#foo}; i++ )); do
   elif [[ "$char" = '+' ]]
   then
     echo "        $previous_mnemonics rax, $num_buffer"
-    tokens+=($num_buffer +)
     previous_mnemonics='add'
     num_buffer=''
     echoerr "detected an addition operator"
   elif [[ "$char" = '-' ]]
   then
     echo "        $previous_mnemonics rax, $num_buffer"
-    tokens+=($num_buffer -)
     previous_mnemonics='sub'
     num_buffer=''
     echoerr "detected a subtraction operator"
@@ -37,5 +58,3 @@ done
 
 echo "        $previous_mnemonics rax, $num_buffer"
 echo '        ret'
-tokens+=($num_buffer)
-echoerr ${tokens[@]}
